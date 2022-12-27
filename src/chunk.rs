8886d0026052059feef1510b0879e7f1417849fd
ARez <dark.gaming@fantasymail.de>
2022-12-27 18:37:35 +0100

    Remove chunk safety processing

--- src/chunk.rs
+++ src/chunk.rs
@@ -8,7 +8,6 @@ pub struct Chunk {
     pub should_step_next_frame: bool,
     pub topleft: IVec2,
     pub size: usize,
-    num_frames_without_step: u8,
 }
 
 
@@ -19,20 +18,11 @@ impl Chunk {
             should_step_next_frame: true,
             topleft,
             size,
-            num_frames_without_step: 0,
         }
     }
 
     pub fn start_step (&mut self) {
         self.should_step = self.should_step_next_frame;
-        if !self.should_step {
-            self.num_frames_without_step += 1;
-
-            if self.num_frames_without_step >= 200 {
-                self.should_step = true;
-                self.num_frames_without_step = 0;
-            };
-        };
         self.should_step_next_frame = false;
     }
 }
